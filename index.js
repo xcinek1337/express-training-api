579371afc9ce577625a92dbbb28cf95e33f1a193
Marcin <xcinek1337@wp.pl>
2023-06-12 13:56:06 +0200

    set path for trains.json

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Nerdbord!')
-})
+	res.send('Hello from Nerdbord!');
+});
 
 app.listen(PORT, () => {
-    console.log('Server listening on port 3000')
-})
+	console.log('Server listening on port 3000');
+});
+
+
+
+
+fs.readFile('./data/trains.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Błąd odczytu pliku:', err);
+    return;
+  }
+
+  const trains = JSON.parse(data);
+
+  console.log(trains);
+});
