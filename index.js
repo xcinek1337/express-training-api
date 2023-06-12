011ab1cd1216b7ebcda6d720ea6b520808a9c661
Marcin <xcinek1337@wp.pl>
2023-06-12 17:10:31 +0200

    get

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const { get } = require('http');
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-	res.send('Hello from Nerdbord!');
+	res.send('Hello from');
 });
 
 app.listen(PORT, () => {
 	console.log('Server listening on port 3000');
 });
 
-
 app.get('/data/trains', (req, res) => {
-    console.log(res);
-    console.log(req);
-  });
+
+});
