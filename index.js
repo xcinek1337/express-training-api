1265529be2f243f6c8f1caec72a1bab2d2c79377
Marcin Chdkowski <marcinchodkowski004@wp.pl>
2025-02-23 15:22:01 +0100

    addition of a post method

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const express = require('express')
-const app = express()
+const { error } = require('console');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-const PORT = process.env.PORT || 4000
+const app = express();
+
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Nerdbord!')
-})
+	res.send('Hello from Nerdbord!');
+});
+
+app.post('/trains', (req, res) => {
+	const filePath = path.join(__dirname, '../data/trains.json');
+
+	if (!req.body) {
+		return res.status(400).json({ error: 'Brak danych' });
+	}
+
+	try {
+		const fileData = fs.readFileSync(filePath, 'utf8');
+		const trains = JSON.parse(fileData);
+
+		const newTrain = {
+			id: trains.length + 1,
+			trainExpressName: req.trainExpressName,
+			countryOfOrigin: req.countryOfOrigin,
+			yearOfConstruction: req.yearOfConstruction,
+			maxKilometerPerHour: req.maxKilometerPerHour,
+			destinationFrom: req.destinationFrom,
+			destinationTo: req.destinationTo,
+		};
+
+		trains.push(newTrain);
+		fs.writeFileSync(filePath, JSON.stringify(trains, null, 2), 'utf-8');
+		res.status(201).json({ message: `Nowy pociąg dodany: ${req.trainExpressName}.` });
+	} catch (error) {
+		console.error('Błąd');
+		res.status(500).json({ error: 'Błąd serwera' });
+	}
+});
 
 app.listen(PORT, () => {
-    console.log('Server listening on port 3000')
-})
+	console.log('Server listening on port 3000');
+});
